272dd0ce22f53593f885533ed30b8a0db5cde81d
Arzym Koteyko <atemiy.kotenko@yandex.ru>
2023-10-13 20:04:39 +0300

    some changes

--- app/static/index.tsx
+++ app/static/index.tsx
@@ -5,11 +5,54 @@ import { App } from './app/app';
 
 
 
+
 const root_tag = document.getElementById('root')
-if (root_tag) {
-    const root = createRoot(root_tag)
-    root.render(
-        <App />
-    )
+
+// ymaps.ready(init);
+async function getRoutes(start, endpoints) {
+    var referencePoints = [start]
+    for (var endpoint of endpoints) {
+        referencePoints.push(endpoint)
+        referencePoints.push(start)
+    }
+    var routes = []
+    function init(ymaps) {
+        var multiRoute = new ymaps.multiRouter.MultiRoute({   
+            // Точки маршрута. Точки могут быть заданы как координатами, так и адресом. 
+            referencePoints: referencePoints
+        });
+        multiRoute.model.events.add('requestsuccess', function() {
+            // Получение ссылки на активный маршрут.
+            var activeRoute = multiRoute.getActiveRoute();
+            var paths = activeRoute.getPaths()
+            paths.each((path, idx) => {
+                idx++;
+                if (idx % 2 == 0) {
+                    routes.push(path)
+                }
+            })
+        });
+        console.log('Done')
+    }
+    await ymaps.ready(init)
+    return routes
 }
-else console.log("Can't find html root tag id to start app")
+
+
+(async () => {
+    if (root_tag) {
+        const root = createRoot(root_tag)
+        let routes = await getRoutes([55.806788, 37.464589], [[55.728783, 37.610988], [55.734876, 37.59308]])
+        console.log(routes)
+        console.log(routes.length)
+        for (let idx=0; idx<routes.length; idx++) {
+            console.log(routes[idx])
+            console.log(routes[idx].properties.get('distance'))
+            console.log(routes[idx].properties.get('duration'))
+        }
+        root.render(
+            <App />
+        )
+    }
+    else console.log("Can't find html root tag id to start app")
+})()
